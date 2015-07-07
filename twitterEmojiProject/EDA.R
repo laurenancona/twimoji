a34f56159401e2ad01c30888389200f8c18c43b1
ctufts <christopher.tufts25@gmail.com>
2015-07-07 15:37:34 -0400

    Date/time converted from string value, emoji frequency counts written to file, percentages of emojis in tweets print to console

--- twitterEmojiProject/EDA.R
+++ twitterEmojiProject/EDA.R
@@ -2,17 +2,25 @@ rm(list =ls())
 library(readxl)
 library(dplyr)
 library(RWeka)
+library(lubridate)
 library(stringr)
 
+# import all tweet data####
 fin <- list.files(path = "data/", full.names = T)
 ds.list <- lapply(fin, read_excel, col_names = F)
 ds <- unique(do.call(rbind.data.frame, ds.list))
+# change column names 
+names(ds) <- c("created","screenName", "text" , "ID", "map.info.A", "map.info.B")
+# convert date
+ds$created <- mdy_hm(ds$created)
+
+# import emoticon table ####
 emoticons <- read.csv("emoticon_conversion_noGraphic.csv", header = F)
+# set colum names
 names(emoticons) <- c("unicode", "bytes","description")
 rm(ds.list)
 rm(fin)
 
-names(ds) <- c("created","screenName", "text" , "ID", "map.info.A", "map.info.B")
 
 # get word frequencies and tokens
 tokens <- WordTokenizer(ds$text)
@@ -41,7 +49,8 @@ emoticons <- cbind(emoji.counts, emoticons)
 
 # get data set of all rows with emojis and identify the emoji type
 # emoji.ds contains all tweet info with description of emoji found
-emoji.indexes <- which(rowSums(emoji.frequency > -1) > 0)
+emoji.per.tweet <- rowSums(emoji.frequency > -1)
+emoji.indexes <- which( emoji.per.tweet > 0)
 emoji.ds <- NULL
 for(i in emoji.indexes){
   
@@ -51,17 +60,28 @@ for(i in emoji.indexes){
   }
 }
 
-# extract x, y coordinates
+# extract x, y coordinates ####
 temp <- str_extract(emoji.ds$map.info.B,"\\d.*(,)-\\d.*(&z=14)")
 locations = strsplit(temp, ",")
-x <- as.numeric(unlist(lapply(locations, function(x)x[1])))
-y <- as.numeric(
+emoji.ds$latitude <- as.numeric(unlist(lapply(locations, function(x)x[1])))
+emoji.ds$longitude <- as.numeric(
   unlist(
     lapply(locations, 
            function(x)strsplit(x[2], "&z")[[1]][1])))
 
-emoji.ds$latitude = x
-emoji.ds$longitude = y
 
-# give us a CSV
+# stats####
+percentage.emoji <- 100*length(emoji.indexes)/nrow(ds)
+cat("percentage of tweets with emoji: ",  round(percentage.emoji,2),"%", sep = "")
+
+# 
+print("Summary of emoji use per tweet (all tweets): \n")
+print(summary(emoji.per.tweet))
+print("Summary of emoji use per tweet (all tweets containing at least one emoji: \n")
+print(summary(emoji.per.tweet[emoji.indexes]))
+
+# write csv containing rows for each unique tweet:emoji combo ####
 write.csv(emoji.ds, file = "twimoji.csv")
+
+# write csv containing frequencies of emojis
+write.csv(arrange(emoticons, desc(emoji.counts)), "emoticon_counts.csv")
